84a487ca60345487deafa358537b34c621c1a90e
s4nt14go <santiagopergamo@gmail.com>
2022-03-17 10:27:19 -0300

    feat(users): move dispatching domain event for aggregate from use case to repo

--- src/modules/users/repos/implementations/fake.ts
+++ src/modules/users/repos/implementations/fake.ts
@@ -2,6 +2,7 @@ import { IUserRepo } from '../userRepo';
 import { UserEmail } from '../../domain/userEmail';
 import { User } from '../../domain/user';
 import { createUser } from '../../testUtils';
+import { DomainEvents } from '../../../../core/domain/events/DomainEvents';
 
 export class UserRepoFake implements IUserRepo {
     findUserByEmail(email: UserEmail): Promise<User> {
@@ -25,6 +26,7 @@ export class UserRepoFake implements IUserRepo {
     };
     save(user: User): Promise<void> {
         if (user.firstName === 'PLEASE FAIL WHEN SAVING') throw Error('Faked failure when saving');
+        DomainEvents.dispatchEventsForAggregate(user.id);   // NOTE: Even though UserRepoFake is being used for unit tests that won't target downstream effects because of triggered event domains, I prefer to put the dispatching just to keep in mind that this should be done after the aggregate changes we're interested in emitting events for, i.e. create, update, delete, are done in the real repository.
         return;
     };
 }
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -8,7 +8,6 @@ import { User } from "../../domain/user";
 import { IUserRepo } from "../../repos/userRepo";
 import { CreateUserErrors } from "./CreateUserErrors";
 import { GenericAppError } from "../../../../core/logic/AppError";
-import { DomainEvents } from '../../../../core/domain/events/DomainEvents';
 
 type Response = Either<
   GenericAppError.UnexpectedError |
@@ -61,8 +60,6 @@ export class CreateUserUseCase implements UseCase<CreateUserDTO, Promise<Respons
     } catch (err) {
       return left(new GenericAppError.UnexpectedError(err)) as Response;
     }
-
-    DomainEvents.dispatchEventsForAggregate(userOrError.getValue().id);
 
     return right(Result.ok<void>()) as Response;
   }
