d0d003cd81938e79e670ea4532ed103096a9d2bd
s4nt14go <santiagopergamo@gmail.com>
2022-06-20 09:35:42 -0300

    fix: IDomainEvent.type is generic

--- src/core/domain/events/IDomainEvent.ts
+++ src/core/domain/events/IDomainEvent.ts
@@ -3,6 +3,6 @@ import { DomainEventTypes } from './DomainEventTypes';
 export interface IDomainEvent {
   dateTimeOccurred: Date;
   aggregateId: string;
-  type: DomainEventTypes.UserCreatedEvent;
+  type: DomainEventTypes;
   version: number;
 }
