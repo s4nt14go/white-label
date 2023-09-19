c51f3ba056f6fd5896cb83adf4ee09c54512b052
s4nt14go <santiagopergamo@gmail.com>
2023-09-19 12:44:01 -0300

    fix: satisfies breaks

--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,16 @@
-import type { SSTConfig } from 'sst';
+// import type { SSTConfig } from 'sst';
 import { MyStack } from './stacks/MyStack';
 
 export default {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  config(_input) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+  config(_input: any) {
     return {
       name: 'white-label',
       region: 'us-east-1',
     };
   },
-  async stacks(app) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async stacks(app: any) {
     app.setDefaultFunctionProps({
       runtime: 'nodejs16.x',  // Node v16 is used all along the project and pipeline
       logRetention: 'two_weeks',
@@ -23,4 +24,4 @@ export default {
     });
     await app.stack(MyStack);
   },
-} satisfies SSTConfig;
+}/* satisfies SSTConfig*/;
